1d1ea65d47dddfcf0bec91a4a80ea537af94dd26
GrigoroiuAlexandru <gheorghealexandru.grigoroiu@ulbsibiu.ro>
2025-03-25 15:22:56 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Random rand = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<Integer> x = new ArrayList();
+        List<Integer> y = new ArrayList();
+
+        for (int i = 0; i < 5; i++) {
+            x.add(rand.nextInt(11));
         }
+        for (int i = 0; i < 7; i++) {
+            y.add(rand.nextInt(11));
+        }
+
+        // Sortarea listelor crescător
+        Collections.sort(x);
+        Collections.sort(y);
+
+        System.out.println("Lista x: " + x);
+        System.out.println("Lista y: " + y);
+
+        // a) xPlusY conține toate elementele din x și y
+        List<Integer> xPlusY = new ArrayList(x);
+        xPlusY.addAll(y);
+        Collections.sort(xPlusY);
+        System.out.println("Lista xPlusY: " + xPlusY);
+
+        // b) zSet conține doar elementele comune între x și y (fără duplicate)
+        Set<Integer> zSet = new TreeSet(x);
+        zSet.retainAll(y);
+        System.out.println("Setul zSet: " + zSet);
+
+        // c) xMinusY conține elementele din x care nu sunt în y
+        List<Integer> xMinusY = new ArrayList(x);
+        xMinusY.removeAll(y);
+        System.out.println("Lista xMinusY: " + xMinusY);
+
+        // d) xPlusYLimitedByP conține elementele din x și y care sunt <= p
+        int p = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList();
+        for (int num : xPlusY) {
+            if (num <= p) {
+                xPlusYLimitedByP.add(num);
+            }
+        }
+        System.out.println("Lista xPlusYLimitedByP: " + xPlusYLimitedByP);
+
     }
 }
